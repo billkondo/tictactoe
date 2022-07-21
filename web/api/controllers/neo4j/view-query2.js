4c5a5a2f635247ca79ab9faff2af454cfa6a9a5f
argit2 <38925828+argit2@users.noreply.github.com>
2022-07-21 20:12:23 -0300

    Improved query table layout Included player usernames on query 'Most recent matches of users you follow' Included more information on query 'Users that who you are following follow the most'

--- web/api/controllers/neo4j/view-query2.js
+++ web/api/controllers/neo4j/view-query2.js
@@ -22,12 +22,18 @@ module.exports = {
     return p1
     order by p1.data DESC`;
     const parameters = {userID : userID};
-    console.log(parameters);
+
     const result = await sails.helpers.queryNeo4J.with({query : query, parameters : parameters });
-    const lines = result.map(record => {
-      return Object.values(record.get('p1').properties);
+    const matchIDs = result.map(record => record.get('p1')?.properties?.matchID);
+
+    const matches = await Promise.all(matchIDs.map(matchID => sails.appDomain.match.findMatch(matchID) ?? {player1: undefined, player2: undefined}));
+    const players = matches.map(match => [match.player1.username, match.player2.username]);
+
+    const lines = result.map((record, index) => {
+      const [player1, player2] = players[index];
+      return [player1, player2, ...Object.values(record.get('p1').properties)];
     })
-    const columnNames = result.length == 0 ? [] : Object.keys(result[0].get('p1').properties);
+    const columnNames = result.length == 0 ? [] : ['player1', 'player2', ...(Object.keys(result[0].get('p1').properties))];
 
     return {
       queryTitle : "Most recent matches of users you follow",
