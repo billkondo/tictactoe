37587d8ab961429800895c99c583e05ea68fdf24
argit2 <38925828+argit2@users.noreply.github.com>
2022-07-21 20:12:23 -0300

    Improved query table layout Included player usernames on query 'Most recent matches of users you follow' Included more information on query 'Users that who you are following follow the most'

--- web/api/controllers/neo4j/view-query3.js
+++ web/api/controllers/neo4j/view-query3.js
@@ -20,19 +20,19 @@ module.exports = {
       const userID = this.req?.user?.userID;
       const query = `MATCH (u1:User {userID: $userID})-[:FOLLOW]->(u2:User), (u2:User)-[:FOLLOW]->(u3:User)
       WHERE u3 <> u1
-      with u3, count(u3) as quantosSeguem
-      return u3
+      with count(u3) as quantosSeguem, u3, collect(u2) as following
+      return u3, quantosSeguem, following
       order by quantosSeguem desc`;
       const parameters = {userID : userID};
       console.log(parameters);
       const result = await sails.helpers.queryNeo4J.with({query : query, parameters : parameters });
       const lines = result.map(record => {
-        return [record.get('u3').properties.username];
+        return [record.get('u3').properties.username, record.get('quantosSeguem'), record.get('following').map(record => record?.properties?.username).join(', ')];
       })
-      const columnNames = ["User"];
+      const columnNames = ["User", "Amount", "Followers that you follow"];
   
       return {
-        queryTitle : "Users that who you are following follows the most",
+        queryTitle : "Users that who you are following follow the most",
         lines : lines,
         columnNames : columnNames
       };
